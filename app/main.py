0a9daf48014cea6e2aab8c2aed0bc32fd697b864
Roberto De Faria <uia23057@contiwan.com>
2025-06-22 12:10:20 -0300

    Ajustar status

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ from datetime import datetime
 from dotenv import load_dotenv
 from openai import OpenAI
 
-# Carrega variáveis de ambiente
+# Carrega variáveis de ambiente #TESTE#
 load_dotenv()
 
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
